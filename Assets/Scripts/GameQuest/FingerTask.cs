86aabeac04ae6f647413d607f45dcaca0e1d63cc
Coman Games <alfaa.gen@gmail.com>
2016-11-05 18:00:44 +0200

    Drawing curve is done all perfect

--- Assets/Scripts/GameQuest/FingerTask.cs
+++ Assets/Scripts/GameQuest/FingerTask.cs
@@ -12,12 +12,41 @@ namespace GameQuest
         private FingerDot _currentDot;
         public Curve OurCurve;
         public VisualTask Visual;
+        public float DistanceToStart = 1f;
         private bool _isFallow;
         private int _currentIndex;
 
         public void Start()
         {
-           Visual.SetActiveAt(0,OurCurve.CurveDots[0]); 
+           Visual.SetActiveAt(0,OurCurve.CurveDots[0]);
+            _isFallow = false;
+            _currentIndex = 0;
+        }
+
+        public void Update()
+        {
+            if(ObjectToFallow.gameObject.activeSelf&&_currentIndex<OurCurve.CurveDots.Length)
+
+                if (!_isFallow)
+                {
+                    float distance = Vector3.Distance(ObjectToFallow.position, OurCurve.CurveDots[0].Point);
+                    Debug.Log("Distance to start"+distance);
+                    if (distance < DistanceToStart)
+                    {
+                        _isFallow = true;
+                        _currentIndex = 1;
+                    }
+                }
+                else
+                {
+                    float current = Vector3.Distance(ObjectToFallow.position, OurCurve.CurveDots[_currentIndex-1].Point);
+                    float next = Vector3.Distance(ObjectToFallow.position, OurCurve.CurveDots[_currentIndex].Point);
+                    if (current>next)
+                    {
+                        _currentIndex++;
+                        Visual.SetActiveAt(_currentIndex,OurCurve.CurveDots[_currentIndex-1]);
+                    }
+                }
         }
 
         public void InitTask()
